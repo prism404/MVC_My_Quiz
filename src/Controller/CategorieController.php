2b05635a3c0420ed8549e0b9202899554ded9fff
gaelWumba <gael.wumba-buatu@epitech.eu>
2022-09-29 13:11:40 +0200

    Yeas

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 
-use App\Repository\CategorieRepository;
+use App\Entity\Categorie;
+
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,22 @@ class CategorieController extends AbstractController
 {
     
     #[Route('/categorie', name: 'app_categorie')]
+<<<<<<< HEAD
     public function index(CategorieRepository $repository): Response
     {
         return $this->render('categorie/index.html.twig', [
             'categorie' => $repository,
+=======
+    public function index(ManagerRegistry  $doctrine): Response
+    {
+        $categorie = $doctrine->getRepository(Categorie::class)->findAll();      
+
+        // dd($categorie);
+
+
+        return $this->render('categorie/categories.html.twig', [
+            'categorie' => $categorie,
+>>>>>>> parent of f87b78b... Test
         ]);
     }
 }
