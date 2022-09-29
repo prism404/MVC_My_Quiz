f5a3b9ba5fbf16cfd694410ba83dd95db69aef60
gaelWumba <gael.wumba-buatu@epitech.eu>
2022-09-29 13:19:58 +0200

    Yeas

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -13,23 +13,10 @@ class CategorieController extends AbstractController
 {
     
     #[Route('/categorie', name: 'app_categorie')]
-<<<<<<< HEAD
     public function index(CategorieRepository $repository): Response
     {
         return $this->render('categorie/index.html.twig', [
             'categorie' => $repository,
-=======
-    public function index(ManagerRegistry  $doctrine): Response
-    {
-        $categorie = $doctrine->getRepository(Categorie::class)->findAll();      
-
-        dd($categorie);
-
-        
-
-        return $this->render('categorie/categories.html.twig', [
-            'categorie' => $categorie,
->>>>>>> parent of f87b78b... Test
         ]);
     }
 }
