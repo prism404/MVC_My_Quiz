1e597f042da2c93e0c740100f00eacc11abec5b6
gaelWumba <gael.wumba-buatu@epitech.eu>
2022-10-02 23:48:11 +0200

    quizz finale...c'etait chaud

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -15,8 +15,19 @@ class CategorieController extends AbstractController
     #[Route('/categorie', name: 'app_categorie')]
     public function index( $repository): Response
     {
+<<<<<<< HEAD
         return $this->render('categorie/index.html.twig', [
             'categorie' => $repository,
+=======
+        $categorie = $doctrine->getRepository(Categorie::class)->findAll();      
+
+        dd($categorie);
+
+        
+
+        return $this->render('categorie/categories.html.twig', [
+            'categorie' => $categorie,
+>>>>>>> parent of dadc8fa... Creation de la page d'inscription
         ]);
     }
 }
